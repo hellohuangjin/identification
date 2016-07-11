bef5f72255d871c1a93f22fffaaf7ee213e4623a
黄金 <huangjin@jinlinbao.com>
2016-07-11 11:14:26 +0800

    simplify program

--- detector.cpp
+++ detector.cpp
@@ -10,7 +10,6 @@ void Detector::end()
     delete api;
 }
 
-
 bool comp(const vector<Point> key1,const vector<Point> key2)
 {
     Rect rect1 = boundingRect(key1);
@@ -21,7 +20,7 @@ bool comp(const vector<Point> key1,const vector<Point> key2)
 string Detector::decoder(Mat image) {
     char *outText;
     string out;
-    api->SetImage((uchar*)image.data, image.size().width, image.size().height, image.channels(), (int)image.step1());
+    api->SetImage(image.data, image.size().width, image.size().height, image.channels(), (int)image.step1());
     outText = api->GetUTF8Text();
     out = outText;
     delete [] outText;
@@ -36,7 +35,8 @@ void Detector::init(const char* datapath)
     {
         printf("Could not initialize tesseract.\n");
     }
-    api->SetVariable("tessedit_char_blacklist", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    api->SetVariable("tessedit_char_blacklist", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[");
+    api->SetPageSegMode(PSM_SINGLE_LINE);
     api->Recognize(NULL);
 }
 
@@ -46,31 +46,49 @@ string Detector::getIdentification(Mat src)
 {
     cv::Mat gray, thr, med, last;
     string text;
-    uint rows = src.rows;
-    vector<Mat> splited;
-    split(src, splited);
-    gray = splited[0];
+    uint rows = (uint)src.rows;
+    vector<Mat> channels;
+    split(src, channels);
 
-    adaptiveThreshold(gray, thr, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 13, 15);
+    gray = channels[0];
 
-    Mat opening = getStructuringElement(MORPH_RECT, Size(rows/800, rows/800));
-    morphologyEx(thr, med, MORPH_OPEN, opening, Point(-1, -1), 2);
+    threshold(gray, thr, 80, 255, THRESH_BINARY_INV);
 
-    Mat closeing = getStructuringElement(MORPH_RECT, Size(rows/20, rows/25));
-    morphologyEx(med, med, MORPH_CLOSE, closeing, Point(-1, -1), 2);
+    Mat closing = getStructuringElement(MORPH_RECT, Size(rows/50, rows/50));
+    morphologyEx(thr, med, MORPH_CLOSE, closing, Point(-1, -1), 4);
+
+    Mat opening = getStructuringElement(MORPH_RECT, Size(2, 2));
+    morphologyEx(med, last, MORPH_OPEN, opening, Point(-1, -1), 4);
 
     vector< vector<Point> > contours;
-    findContours(med, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+    findContours(last, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
     sort(contours.begin(), contours.end(), comp);
 
     size_t szie = contours.size();
     Rect rect = boundingRect(contours[szie-1]);
-    Mat tmp(gray, rect);
-    adaptiveThreshold(tmp, tmp, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 57, 15);
+    if (rect.x > 10) {
+        rect.x -= 10;
+        rect.width += 10;
+    }
+    if (rect.y > 10) {
+        rect.y -= 10;
+        rect.height += 10;
+    }
+
+    Mat tmp(thr, rect);
     text = decoder(tmp);
     rect = boundingRect(contours[0]);
-    Mat atmp(gray, rect);
+
+    if (rect.x > 10) {
+        rect.x -= 10;
+        rect.width += 20;
+    }
+    if (rect.y > 10) {
+        rect.y -= 10;
+        rect.height += 10;
+    }
+
+    Mat atmp(thr, rect);
     text += decoder(atmp);
-    adaptiveThreshold(atmp, atmp, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 57, 15);
     return  text;
 }
